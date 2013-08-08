6d4d8c2518d5509f8871380fcf9499a0ea3deeb4
Laurens Van Houtven <_@lvh.cc>
2013-08-08 14:47:06 +0200

    Add automated testing tools

---
+++ setup.py
@@ -0,0 +1,53 @@
+import sys
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+import re
+versionLine = open("ctfclient/_version.py", "rt").read()
+match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", versionLine, re.M)
+versionString = match.group(1)
+
+class Tox(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import tox
+        sys.exit(tox.cmdline([]))
+
+setup(name='ctfclient',
+      version=versionString,
+      description='Client for the game that comes with Crypto 101, the '
+                  'introductory book on cryptography.',
+      long_description='The game client for an online, hacker-themed text '
+                       'adventure revolving around breaking cryptosystems. '
+                       'Built using Twisted and Urwid.',
+      url='https://github.com/lvh/ctfclient',
+
+      author='Laurens Van Houtven',
+      author_email='_@lvh.io',
+
+      packages=["ctfclient", "ctfclient.test"],
+      test_suite="ctfclient.test",
+      setup_requires=['tox'],
+      cmdclass={'test': Tox},
+      zip_safe=True,
+
+      license='ISC',
+      keywords="crypto urwid twisted",
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Framework :: Twisted",
+        "Intended Audience :: Education",
+        "License :: OSI Approved :: ISC License (ISCL)",
+        "Programming Language :: Python :: 2 :: Only",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Education",
+        "Topic :: Games/Entertainment",
+        "Topic :: Security :: Cryptography",
+        ]
+)
