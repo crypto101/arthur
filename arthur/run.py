6b2e2b879166ec1acd81850e3a8b06809391d006
Laurens Van Houtven <_@lvh.cc>
2013-11-27 20:38:14 +0100

    Move meat and potatoes of demo to a module

---
+++ arthur/run.py
@@ -0,0 +1,28 @@
+import urwid
+from arthur import exercises, ui
+
+
+def buildWorkbench():
+    """Builds a workbench.
+
+    The workbench has a launcher with all of the default tools. The
+    launcher will be displayed on the workbench.
+
+    """
+    workbench = ui.Workbench()
+
+    tools = [exercises.SearchTool()]
+    launcher = ui.Launcher(workbench, tools)
+    workbench.display(launcher)
+
+    return workbench
+
+
+def buildMainLoop(workbench):
+    """Builds a main loop from the given workbench.
+
+    The main loop will have the default pallette, as well as the
+    default unused key handler.
+
+    """
+    urwid.MainLoop(workbench.widget, ui.DEFAULT_PALETTE)
