7b8c45c1211e3c75a8d7aa499786a6f5297d0576
Laurens Van Houtven <_@lvh.cc>
2013-12-01 11:23:48 +0100

    projectName -> packageName

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ import sys
 from setuptools import setup
 from setuptools.command.test import test as TestCommand
 
-projectName = "arthur"
+packageName = "arthur"
 
 import re
-versionLine = open("{0}/_version.py".format(projectName), "rt").read()
+versionLine = open("{0}/_version.py".format(packageName), "rt").read()
 match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", versionLine, re.M)
 versionString = match.group(1)
 
@@ -19,7 +19,7 @@ class Tox(TestCommand):
         import tox
         sys.exit(tox.cmdline([]))
 
-setup(name=projectName,
+setup(name=packageName,
       version=versionString,
       description='Software for the exercises in Crypto 101, the introductory '
                   'book on cryptography.',
