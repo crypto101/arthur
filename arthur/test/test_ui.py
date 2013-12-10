9aa566656c9eea299c28fbe579768dcb29a023c4
Laurens Van Houtven <_@lvh.cc>
2013-12-10 18:19:04 +0100

    Added undisplay feature to the FakeWorkbench

--- arthur/test/test_ui.py
+++ arthur/test/test_ui.py
@@ -193,12 +193,16 @@ class FakeWorkbench(object):
         self.tools = []
 
 
+    def undisplay(self):
+        self.tools.pop()
+
+
     def display(self, tool):
         self.tools.append(tool)
 
 
     def clear(self):
-        self.tools = []
+        self.tools.clear()
 
 
 
