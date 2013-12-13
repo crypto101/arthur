6a2bade44ab62d484116f3dbc1937bb48ab3e488
Laurens Van Houtven <_@lvh.cc>
2013-12-13 15:17:18 +0100

    Solved exercise notification

--- arthur/exercises.py
+++ arthur/exercises.py
@@ -1,7 +1,9 @@
 """
 UI tools related to exercise search and selection.
 """
-from arthur.ui import _Prompt
+from arthur.ui import _Prompt, notify
+from clarent.exercise import NotifySolved
+from twisted.protocols import amp
 
 
 class SearchTool(_Prompt):
@@ -10,3 +12,19 @@ class SearchTool(_Prompt):
 
     def __init__(self):
         _Prompt.__init__(self, self.name, u"Search terms: ")
+
+
+
+class ExercisesLocator(amp.CommandLocator):
+    def __init__(self, workbench):
+        self.workbench = workbench
+
+
+    @NotifySolved.responder
+    def notifySolved(self, _identifier, title):
+        """Notifies the user that a particular exercise has been solved.
+
+        """
+        notify(self.workbench, u"Congratulations", u"Congratulations! You "
+               "have completed the '{title}' exercise.".format(title=title))
+        return {}
---
+++ arthur/test/test_exercises.py
@@ -0,0 +1,42 @@
+"""Tests for exercises-related code. """
+from arthur import exercises
+from arthur.test.fakes import FakeWorkbench
+from clarent.exercise import NotifySolved
+from twisted.trial.unittest import SynchronousTestCase
+from txampext.respondertests import ResponderTestMixin
+
+
+class NotifySolvedResponderTests(SynchronousTestCase, ResponderTestMixin):
+    """The exercise responder locator has a responder for NotifySolved.
+
+    """
+    command = NotifySolved
+    locator = exercises.ExercisesLocator(None)
+
+
+
+class NotifySolvedTests(SynchronousTestCase):
+    """Tests for exercise solution notifications.
+
+    """
+    def setUp(self):
+        self.workbench = FakeWorkbench()
+        self.responder = exercises.ExercisesLocator(self.workbench)
+
+
+    def test_notifySolved(self):
+        """The user receives a notification when an exercise is completed. The
+        notification can be clicked away.
+
+        """
+        self.assertEqual(self.workbench.tools, [])
+
+        result = self.responder.notifySolved(b"identifier", u"The Exercise")
+        self.assertEqual(result, {})
+
+        notification = self.workbench.tools[-1]
+        self.assertIn(u"Congratulations", notification.text)
+        self.assertIn(u"The Exercise", notification.text)
+
+        notification.button.keypress((1,), " ")
+        self.assertEqual(self.workbench.tools, [])
